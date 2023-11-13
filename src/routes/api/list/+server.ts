97113ccafc871e81eb7b5d8d4ea446c196df9fe2
jason wong <jayywong92@gmail.com>
2023-11-12 16:24:18 -0800

    replace manual query logic with sql

--- src/routes/api/list/+server.ts
+++ src/routes/api/list/+server.ts
@@ -1,6 +1,31 @@
 import Anilist, { type ListEntry } from "anilist-node";
+import { Connection, Database } from "duckdb-async";
 
 const anilist = new Anilist();
+
+async function getAniList(username: string | number, conn: Connection) {
+  const stmt = await conn.prepare(
+    "INSERT INTO anime VALUES (?, ?, ?, ?, ?, ?)",
+  );
+  const query = await anilist.lists.anime(username);
+  const entries: ListEntry[] = [];
+  query.forEach((e) => {
+    entries.push(...e.entries);
+  });
+  entries.forEach((e) => {
+    const title =
+      e.media.title.english ??
+      e.media.title.romaji ??
+      e.media.title.native ??
+      "";
+    const score = e.score ?? 0;
+    const url = e.media.siteUrl ?? "";
+    const status = e.status ?? "";
+    const idMal = e.media.idMal?.toString() ?? "";
+    stmt.run(username, title, score, url, status, idMal);
+  });
+  await stmt.finalize();
+}
 
 export async function POST({ request }) {
   const options: ResponseInit = {
@@ -8,67 +33,46 @@ export async function POST({ request }) {
   };
   try {
     const { u1, u2 } = await request.json();
-    const queries = await Promise.all([
-      anilist.lists.anime(u1),
-      anilist.lists.anime(u2),
+    if ([u1, u2].filter((element) => element === undefined).length != 2) {
+      throw new Error("incorrect number of username(s)");
+    }
+
+    const db = await Database.create(":memory:");
+    await db.exec(`
+      CREATE TABLE "anime"(
+        "username" VARCHAR(255),
+        "title" VARCHAR(255),
+        "score" INTEGER,
+        "url" VARCHAR(255),
+        "status" VARCHAR(255),
+        "idMal" VARCHAR(255),
+      );
+    `);
+
+    await Promise.all([
+      getAniList(u1, await db.connect()),
+      getAniList(u2, await db.connect()),
     ]);
-    const combined: ListEntry[][] = [[], []];
-    queries.forEach((e, i) => {
-      e.forEach((e) => {
-        combined[i].push(...e.entries);
-      });
-    });
 
-    // convert to a list of objects that only has the title, score, url, and status
-    const converted = combined.map((userList: ListEntry[]) => {
-      return userList.map((e: ListEntry) => {
-        const entry: {
-          title: string;
-          score: number;
-          url: string;
-          status: string;
-        } = {
-          title: e.media.title.english,
-          score: e.score,
-          url: e.media.siteUrl,
-          status: e.status,
-        };
-        return entry;
-      });
-    });
+    const stmt = await db.prepare(`
+      SELECT *
+      FROM anime AS e1
+      WHERE e1.username = ?
+      AND NOT EXISTS (
+        SELECT 1
+        FROM anime AS e2
+        WHERE e2.username = ?
+        AND e2.idMal = e1.idMal
+      ) order by title;
+    `);
+    const result1 = await stmt.all(u1, u2);
+    const result2 = await stmt.all(u2, u1);
 
-    const combinedSets = converted.map((userList) => {
-      const keyedSet: {
-        [key: string]: {
-          title: string;
-          score: number;
-          url: string;
-          status: string;
-        };
-      } = {};
-      userList.forEach((e) => {
-        keyedSet[e.title] = e;
-      });
-      return keyedSet;
-    });
-
-    const uniqueTo1 = Object.values(combinedSets[0]).filter(
-      (e) => !(e.title in combinedSets[1]),
-    );
-    const uniqueTo2 = Object.values(combinedSets[1]).filter(
-      (e) => !(e.title in combinedSets[0]),
-    );
-
-    // sort by title
-    uniqueTo1.sort((a, b) => a.title.localeCompare(b.title));
-    uniqueTo2.sort((a, b) => a.title.localeCompare(b.title));
-    return new Response(
-      JSON.stringify({ u1: uniqueTo1, u2: uniqueTo2 }),
-      options,
-    );
+    return new Response(JSON.stringify({ u1: result1, u2: result2 }), options);
   } catch (e: unknown) {
     if (e instanceof Error) {
       options.status = 500;
+      console.error(e);
       return new Response(JSON.stringify({ error: e.message }), options);
     }
     throw e;
